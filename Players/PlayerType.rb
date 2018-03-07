0c124910b7d5791a4ed2ccb0bb68f82e92934561
Rani Zilpelwar <ranizilpelwar@Ranis-MacBook-Pro.local>
2018-03-07 09:32:00 -0500

    Use attr_reader
    Utilized attr_reader to create a wrapper for the options variable

--- Players/PlayerType.rb
+++ Players/PlayerType.rb
@@ -1,10 +1,16 @@
 require_relative '../Exceptions/PlayerTypeError.rb'
 
 class PlayerType
-  options = [ :Human, :Computer ]
+  attr_reader :options
+
+  def options
+    @options = [ :Human, :Computer ]
+  end
+  
   def initialize(type)
     if type != :Human and type != :Computer
       raise PlayerTypeError.new("PlayerType has only two valid choices: Human, Computer.") 
     end
   end
+
 end
