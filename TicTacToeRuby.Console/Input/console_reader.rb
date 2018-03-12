14014a8ccdb9e0f2a000e3e6c6baf5c024c5f1dd
Rani Zilpelwar <ranizilpelwar@Ranis-MacBook-Pro.local>
2018-03-12 09:53:57 -0400

    Add comment
    Add comment to explain the method - it doesn't read the enter key

--- TicTacToeRuby.Console/Input/console_reader.rb
+++ TicTacToeRuby.Console/Input/console_reader.rb
@@ -1,4 +1,6 @@
 class ConsoleReader
+  
+  # This will read the input from the user without storing the Enter key input that the user types at the end.
   def read_input
     input = gets.chomp
   end
