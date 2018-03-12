120b4f4ad6f9f624f3ec4c89630f86257304860d
Rani Zilpelwar <ranizilpelwar@Ranis-MacBook-Pro.local>
2018-03-12 13:13:41 -0400

    Fix bug with identifying a digit
    Added a unit test to validate that the symbol selected by the user was not a digit. Found a bug with the logic and fixed it so that a valid symbol excludes digits.

--- TicTacToeRuby.Core.UnitTests/Validators/tc_player_symbol_validation.rb
+++ TicTacToeRuby.Core.UnitTests/Validators/tc_player_symbol_validation.rb
@@ -2,9 +2,23 @@ require 'test/unit'
 require_relative '../../TicTacToeRuby.Core/Validators/player_symbol_validator.rb'
 
 class TestPlayerSymbolValidation < Test::Unit::TestCase
-  def test_when_input_is_an_empty_string_then_false_is_returned
+  def test_an_empty_string_is_not_a_valid_player_symbol
     input = ""
     return_value = PlayerSymbolValidator.valid?(input)
-    assert_false(return_value, "False should be returned when input is an empty string.")
+    assert_false(return_value, "An empty string should not be a valid player symbol.")
   end
+
+  def test_a_digit_is_not_a_valid_player_symbol
+    input = "1"
+    return_value = PlayerSymbolValidator.valid?(input)
+    assert_false(return_value, "A digit should not be a valid symbol.")
+  end
+
+  def test_input_with_length_greater_than_one_is_not_a_valid_player_symbol
+    input = "XX"
+    return_value = PlayerSymbolValidator.valid?(input)
+    assert_false(return_value, "An input with length greater than one is not a valid symbol.")
+  end
+
+  
 end
