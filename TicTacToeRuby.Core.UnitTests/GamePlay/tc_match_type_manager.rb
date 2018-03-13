0ed3efc574739a12ca2c14636c51f871639c15d7
Rani Zilpelwar <ranizilpelwar@Ranis-MacBook-Pro.local>
2018-03-13 09:43:28 -0400

    Added MatchTypeManager
    Added class to manage the types of matches available to play in the game.
    Created associated unit tests.

---
+++ TicTacToeRuby.Core.UnitTests/GamePlay/tc_match_type_manager.rb
@@ -0,0 +1,38 @@
+require 'test/unit'
+require_relative '../../TicTacToeRuby.Core/GamePlay/match_type_manager.rb'
+require_relative '../../TicTacToeRuby.Core/GamePlay/match_type.rb'
+require_relative '../../TicTacToeRuby.Core/Players/player_type.rb'
+
+class TestMatchTypeManager < Test::Unit::TestCase
+  def test_initialize_creates_a_total_of_three_match_types
+    expected_length = 3
+    match_type_manager = MatchTypeManager.new
+    actual_length = match_type_manager.matches.length
+    assert_equal(expected_length, actual_length, "Total 3 matches should be created.")
+  end
+
+  def test_get_total_available_matches_should_return_a_value_of_three
+    expected_total = 3
+    match_type_manager = MatchTypeManager.new
+    actual_total = match_type_manager.get_total_available_matches
+    assert_equal(expected_total, actual_total)
+  end
+
+  def test_when_match_number_is_provided_as_zero_then_argument_error_is_raised
+    match_type_manager = MatchTypeManager.new
+    assert_raises ArgumentError do match_type_manager.get_match_type(0) end
+  end
+
+  def test_when_match_number_is_provided_as_four_then_argument_error_is_raised
+    match_type_manager = MatchTypeManager.new
+    assert_raises ArgumentError do match_type_manager.get_match_type(4) end
+  end
+
+  def test_when_match_number_is_one_then_human_vs_human_match_type_is_returned
+    expected_player1_type = :Human
+    match_type_manager = MatchTypeManager.new
+    actual_match_type = match_type_manager.get_match_type(1)
+    actual_player1_type = actual_match_type.player1_type.selected_option
+    assert_equal(expected_player1_type, actual_player1_type, "Player 1 type for first match should be Human.")
+  end
+end
