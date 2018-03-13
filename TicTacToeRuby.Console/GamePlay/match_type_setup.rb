e5b26e228f84a848773bd2399a514d64c0b1fed6
Rani Zilpelwar <ranizilpelwar@Ranis-MacBook-Pro.local>
2018-03-13 11:50:30 -0400

    Complete get_valid_match_type
    Completed the method implementation so that it validates the provided value and repeats the statement if invalid (so the user can try again).
    Used tc_test.rb to test out the statements printed to the terminal.

--- TicTacToeRuby.Console/GamePlay/match_type_setup.rb
+++ TicTacToeRuby.Console/GamePlay/match_type_setup.rb
@@ -2,6 +2,7 @@ require_relative '../../TicTacToeRuby.Core/GamePlay/match_type_manager.rb'
 require_relative '../../TicTacToeRuby.Core/GamePlay/match_type.rb'
 require_relative '../../TicTacToeRuby.Core/Players/player_type.rb'
 require_relative '../Output/console_writer.rb'
+require_relative '../Input/console_reader.rb'
 
 class MatchTypeSetup
 
@@ -9,8 +10,17 @@ class MatchTypeSetup
   # Validates that the selection is within range of available matches.
   def self.get_valid_match_type(writer, reader, match_type_manager)
     input = ""
-    is_valid = false
+    valid = false
+    total_matches = match_type_manager.get_total_available_matches
+    prompt_for_match_type_selection(writer, match_type_manager)
 
+    while !valid
+      input = reader.read_input_ignore_empty
+      numeric_input = input.to_i
+      valid = match_type_manager.valid?(numeric_input)
+      writer.display_message("Oops! I couldn't use that. Type a number from 1 to #{total_matches}.") unless valid
+    end 
+    match_type = match_type_manager.get_match_type(numeric_input)
   end
 
   def self.prompt_for_match_type_selection(writer, match_type_manager)
@@ -22,7 +32,6 @@ class MatchTypeSetup
       writer.display_message("#{type}. #{player1_type_selection} Vs #{player2_type_selection}")  
       type = type + 1
     }
-
   end
 end
 
--- TicTacToeRuby.Console/GamePlay/tc_test.rb
+++ TicTacToeRuby.Console/GamePlay/tc_test.rb
@@ -5,9 +5,10 @@ require_relative 'match_type_setup.rb'
 puts "Testing the test scripts:"
 
 writer = ConsoleWriter.new
+reader = ConsoleReader.new
 match_type_manager = MatchTypeManager.new
-MatchTypeSetup.prompt_for_match_type_selection(writer, match_type_manager)
-
+#MatchTypeSetup.prompt_for_match_type_selection(writer, match_type_manager)
+MatchTypeSetup.get_valid_match_type(writer, reader, match_type_manager)
 
 
 
