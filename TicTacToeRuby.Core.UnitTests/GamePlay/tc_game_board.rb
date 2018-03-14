7a9f9e131c6dde86794645300b00a71142605274
Rani Zilpelwar <ranizilpelwar@Ranis-MacBook-Pro.local>
2018-03-14 09:58:09 -0400

    Enhance GameBoard
    Added a static method to create a default game board to ease of use - this can be used during test writing to use the sample board provided and can be passed in during GameBoard initialization.
    Wrote assoc unit tests.
    Added update_board message with error handling for bad parameters. Using PlayerSymbolValidator class to guard against bad player_symbol inputs.
    Wrote assoc unit tests.

--- TicTacToeRuby.Core.UnitTests/GamePlay/tc_game_board.rb
+++ TicTacToeRuby.Core.UnitTests/GamePlay/tc_game_board.rb
@@ -3,14 +3,42 @@ require_relative '../../TicTacToeRuby.Core/GamePlay/game_board.rb'
 require_relative '../Players/mock_player_manager.rb'
 
 class TestGameBoard < Test::Unit::TestCase
+
+  attr_reader :game_board, :board, :player_manager
+  def setup
+    @board = GameBoard.create_board
+    @player_manager = MockPlayerManager.new
+    @game_board = GameBoard.new(@player_manager, @board)
+  end
+
   def test_initialize_will_raise_an_exception_when_player_manager_is_nil
     assert_raises(ArgumentError) do GameBoard.new(nil) end
   end
 
-  def test_initialize_board_will_set_default_nine_element_board
-    mock_player_manager = MockPlayerManager.new
-    game_board = GameBoard.new(mock_player_manager)
+  def test_create_board_will_create_a_nine_element_board
     expected_board_length = 9
-    assert_equal(expected_board_length, game_board.board.length, "Expected default board to have 9 elements.")
+    assert_equal(expected_board_length, @board.length, "Expected default board to have 9 elements.")
   end
+
+  def test_initialize_will_set_the_board_to_the_provided_parameter
+    assert_equal(@board, @game_board.board, "Expected game board's board to match provided board.")
+  end
+
+  def test_initialize_will_set_the_player_manager_to_the_provided_parameter
+    assert_equal(@player_manager, @game_board.player_manager, 
+      "Expected game board's player manager to match the provided one.")
+  end
+
+  def test_update_board_raises_argument_error_when_game_piece_is_nil
+    assert_raises(ArgumentError) do @game_board.update_board(0, nil) end
+  end
+
+  def test_update_board_raises_argument_error_when_index_is_length_of_board
+    assert_raises(ArgumentError) do @game_board.update_board(9, @board) end
+  end
+
+  def test_update_board_raises_argument_error_when_index_is_greater_than_length_of_board
+    assert_raises(ArgumentError) do @game_board.update_board(-1, @board) end
+  end
+
 end
--- TicTacToeRuby.Core/GamePlay/game_board.rb
+++ TicTacToeRuby.Core/GamePlay/game_board.rb
@@ -1,19 +1,32 @@
 require_relative '../Players/player_manager.rb'
+require_relative '../Validators/player_symbol_validator.rb'
 
 class GameBoard
   attr_reader :player_manager, :board
 
-  def initialize(player_manager)
+  def initialize(player_manager, board)
     raise ArgumentError, "Cannot create GameBoard with nil player_manager." unless player_manager != nil
-    @board = []
-    @board << "1"
-    @board << "2"
-    @board << "3"
-    @board << "4"
-    @board << "5"
-    @board << "6"
-    @board << "7"
-    @board << "8"
-    @board << "9"
+    @player_manager = player_manager
+    @board = board
+  end
+
+  def self.create_board
+    board = []
+    board << "1"
+    board << "2"
+    board << "3"
+    board << "4"
+    board << "5"
+    board << "6"
+    board << "7"
+    board << "8"
+    board << "9"
+  end
+
+  def update_board(index, player_symbol)
+    raise ArgumentError, "Cannot update board with invalid player_symbol" unless PlayerSymbolValidator.valid?
+    raise ArgumentError, "Index provided cannot be greater than the last spot on the board." unless index < board.length
+    raise ArgumentError, "Index provided cannot be less than the first spot on the board." unless index > 0
+    
   end
 end
