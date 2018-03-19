67152d0ed059278fc9bfbd95f2553b865fd9ba96
Rani Zilpelwar <ranizilpelwar@Ranis-MacBook-Pro.local>
2018-03-19 09:05:15 -0400

    Clean up formatting for better readability
    Remove unncessary ternary operation
    Replace unless with if

--- TicTacToeRuby.Core/Validators/available_spaces_validator.rb
+++ TicTacToeRuby.Core/Validators/available_spaces_validator.rb
@@ -1,10 +1,10 @@
 class AvailableSpacesValidator
 
   def self.get_available_spaces(board)
-    raise ArgumentError, "Cannot get available spaces when provided board is nil." unless board.nil? == false
+    raise ArgumentError, "Cannot get available spaces when provided board is nil." if board.nil?
     available_spaces = []
     board.each do |tile_on_board| 
-      available_spaces.push(tile_on_board) unless AvailableSpacesValidator.digit?(tile_on_board) == false
+      available_spaces.push(tile_on_board) unless !AvailableSpacesValidator.digit?(tile_on_board)
     end
     return available_spaces
   end
@@ -13,6 +13,6 @@ class AvailableSpacesValidator
   def self.digit?(input)
     pattern = /\A[0-9]+\z/
     match_data = pattern.match(input)
-    valid = match_data !=nil ? true : false
+    valid = match_data !=nil
   end
 end
