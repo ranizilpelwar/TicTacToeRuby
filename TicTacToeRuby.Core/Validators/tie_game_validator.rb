80f12bcb087485ce42908786a890f1e8d37a2f6e
Rani Zilpelwar <ranizilpelwar@Ranis-MacBook-Pro.local>
2018-03-19 09:07:58 -0400

    Clean up formatting for better readability
    Remove unncessary ternary operation
    Replace unless with if

--- TicTacToeRuby.Core/Validators/tie_game_validator.rb
+++ TicTacToeRuby.Core/Validators/tie_game_validator.rb
@@ -2,8 +2,8 @@ require_relative 'available_spaces_validator.rb'
 
 class TieGameValidator
   def self.tie_game?(board)
-    raise ArgumentError, "Cannot determine if game is tied because provided board is nil." unless !board.nil?
+    raise ArgumentError, "Cannot determine if game is tied because provided board is nil." if board.nil?
     available_spaces = AvailableSpacesValidator.get_available_spaces(board)
-    result = available_spaces.length == 0 ? true : false
+    result = available_spaces.length == 0
   end
 end
