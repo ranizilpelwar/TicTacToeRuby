db08650e48479646c79e79a307157aea29193e3d
Rani Zilpelwar <ranizilpelwar@Ranis-MacBook-Pro.local>
2018-03-19 13:50:49 -0400

    Fix a bug with AvailableSpacesValidator
    Fixed a bug with the code where it was not returning the proper type of elements (index instead of tile).
    Added a unit test for it.

--- TicTacToeRuby.Core.UnitTests/Validators/tc_available_spaces_validation.rb
+++ TicTacToeRuby.Core.UnitTests/Validators/tc_available_spaces_validation.rb
@@ -60,6 +60,23 @@ class TestAvailableSpacesValidation < Test::Unit::TestCase
     assert_equal(expected_result, actual_result, "Errantly got #{actual_result} element(s) and list contains: #{available_spaces}.")
   end
 
+  def test_get_available_spaces_contains_expected_index_when_there_is_only_one_available_spot_on_the_board
+    board = []
+    board << "X"
+    board << "X"
+    board << "X"
+    board << "X"
+    board << "X"
+    board << "X"
+    board << "X"
+    board << "X"
+    board << "9"
+    expected_result = 8
+    available_spaces = AvailableSpacesValidator.get_available_spaces(board)
+    actual_result = available_spaces[0]
+    assert_equal(expected_result, actual_result, "Errantly got #{actual_result} as the index when expected to get #{expected_result}.")
+  end
+
   def test_get_available_spaces_contains_zero_elements_when_there_are_no_available_spots_on_the_board
     board = []
     board << "X"
