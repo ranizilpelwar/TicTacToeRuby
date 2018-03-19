33d5c392c32b964a6a72b37a35563eb66fe08496
Rani Zilpelwar <ranizilpelwar@Ranis-MacBook-Pro.local>
2018-03-19 09:49:52 -0400

    Remove unncessary ternary operator
    For better readability and removal of duplication of code

--- TicTacToeRuby.Core/Validators/player_symbol_validator.rb
+++ TicTacToeRuby.Core/Validators/player_symbol_validator.rb
@@ -2,6 +2,6 @@ class PlayerSymbolValidator
   def self.valid?(input)
     pattern = /\A[^0-9\s]{1}\z/
     match_data = pattern.match(input)
-    valid = match_data !=nil ? true : false
+    match_data != nil
   end
 end
