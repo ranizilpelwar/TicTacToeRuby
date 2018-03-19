f0d69e93caa06202d602f8c8c0887a9f84b9d23a
Rani Zilpelwar <ranizilpelwar@Ranis-MacBook-Pro.local>
2018-03-19 13:01:25 -0400

    Added initialize method
    Added an initialize method that takes in the index and score

--- TicTacToeRuby.Core/GamePlay/weighted_move.rb
+++ TicTacToeRuby.Core/GamePlay/weighted_move.rb
@@ -2,4 +2,9 @@ class WeightedMove
   # index: Spot on board where move is made.
   # score: Value of move made on board.
   attr_accessor :index, :score
+
+  def initialize(index, score)
+    @index = index
+    @score = score
+  end
 end
