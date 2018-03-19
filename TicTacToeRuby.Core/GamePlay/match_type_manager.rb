711fa083a17f18c7631937a1f26d2e3e447bb6d8
Rani Zilpelwar <ranizilpelwar@Ranis-MacBook-Pro.local>
2018-03-19 08:48:58 -0400

    Clean up unncessary check
    Cleaned up code to remove unncessary ternary operator as the condition itself provided the boolean result.

--- TicTacToeRuby.Core/GamePlay/match_type_manager.rb
+++ TicTacToeRuby.Core/GamePlay/match_type_manager.rb
@@ -36,6 +36,6 @@ class MatchTypeManager
 
   def valid?(match_number)
     max_match_number = get_total_available_matches
-    valid = match_number >= 1 && match_number <= max_match_number ? true : false
+    valid = match_number >= 1 && match_number <= max_match_number
   end
 end
