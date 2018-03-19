5202d9f00f1d7a455767fd3cc24102903aa02cd3
Rani Zilpelwar <ranizilpelwar@Ranis-MacBook-Pro.local>
2018-03-19 14:54:46 -0400

    Completed ComputerActions
    Completed the implementation of methods in this class. get_best_move calculates the best move the player should make based on a score keeping calculation. It uses the Minimax algorithm using alpha-beta pruning to improve the performance of the recursive approach.

---
+++ TicTacToeRuby.Core/GamePlay/computer_actions.rb
@@ -0,0 +1,75 @@
+require_relative 'game_board.rb'
+require_relative 'weighted_move.rb'
+require_relative '../Players/player_manager.rb'
+require_relative '../Validators/available_spaces_validator.rb'
+require_relative '../Validators/board_score_validator.rb'
+require_relative '../Validators/game_over_validator.rb'
+require_relative '../Validators/player_symbol_validator.rb'
+
+class ComputerActions
+  attr_reader :current_player, :other_player, :game_board
+
+  def initialize(game_board)
+    raise ArgumentError, "Cannot create ComputerActions with nil game_board." if game_board.nil?
+    @game_board = game_board
+    @player_manager = @game_board.player_manager
+    @current_player = @player_manager.current_player.symbol
+    @other_player = @player_manager.get_next_player.symbol
+  end
+
+  def get_best_move(board, player_symbol, depth, best_max_value, best_min_value)
+    raise ArgumentError, "Cannot get best move because board is nil." if board.nil?
+    raise ArgumentError, "Cannot get best move because player_symbol is invalid." if !PlayerSymbolValidator.valid?(player_symbol)
+    next_moves = AvailableSpacesValidator.get_available_spaces(board)
+    tile = ""
+    current_score = 0
+    best_move = -1
+    if next_moves.size == 0 || depth == 0
+      current_score = BoardScoreValidator.evaluate_score_of_board(board, current_player, other_player)
+      move = WeightedMove.new(best_move, current_score)
+      return move
+    else
+      next_moves.each do |move|
+	      tile = board[move]
+        board[move] = player_symbol
+      	if player_symbol == current_player
+          i_won = GameOverValidator.win_for_player?(player_symbol, board)
+      	  if i_won 
+            # An early win has an advantage:
+      	    current_score = BoardScoreValidator.evaluate_score_of_board(board, current_player, other_player) * depth
+      	  else
+      	    current_score = get_best_move(board, other_player, depth - 1, best_max_value, best_min_value).score
+          end
+          if current_score > best_max_value
+            best_max_value = current_score
+            best_move = move
+      	  end
+      	else
+    	    he_won = GameOverValidator.win_for_player?(player_symbol, board)
+    	    if he_won
+            # A block is an advantage:
+    	      current_score = BoardScoreValidator.evaluate_score_of_board(board, current_player, other_player) * depth
+    	    else
+            current_score = get_best_move(board, current_player, depth - 1, best_max_value, best_min_value).score
+    	    end
+    	    if current_score < best_min_value
+    	      best_min_value = current_score
+    	      best_move = move
+    	    end
+      	end
+    	  #Reset the board tile:
+        board[move] = tile
+    	  #Stop traversing more nodes in this branch:
+    	  break if best_max_value >= best_min_value
+      end
+    end
+    if player_symbol == current_player
+      score = best_max_value
+    else
+      score = best_min_value
+    end
+    weighted_move = WeightedMove.new(best_move, score)
+    return weighted_move
+  end
+end
+
