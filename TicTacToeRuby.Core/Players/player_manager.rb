b26af7e607c20504c8890dd834601be62f100420
ranizilpelwar <ranizilpelwar@gmail.com>
2018-03-20 13:40:01 -0400

    Implement get_player_number + tests
    Implement method that identifies the player number for the provided player.

--- TicTacToeRuby.Core/Players/player_manager.rb
+++ TicTacToeRuby.Core/Players/player_manager.rb
@@ -15,4 +15,17 @@ class PlayerManager
   def get_next_player
     next_player = @current_player.equals?(@player1) ? @player2 : @player1
   end
+
+  # Returns the number of the player stored in Player Manager, where 1 represents player 1 and -1 indicates that the player could not be found.
+  def get_player_number(player)
+    raise ArgumentError, "Provided player cannot be nil." if player.nil?
+    number = 
+      if player.equals?(@player1) 
+        1
+      elsif player.equals?(@player2) 
+        2
+      else 
+        -1
+    end
+  end
 end
