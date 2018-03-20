6c78fd48bbd40f49408b1d06d00e26b8d2d3dc6a
ranizilpelwar <ranizilpelwar@gmail.com>
2018-03-20 15:35:46 -0400

    Fix logic in TieGame
    "tie_game?" was changed to return false when there is a winning configuration on the board. It was previously only checking for unoccupied tiles.
    Added/modified tests to make sure both cases are checked.

--- TicTacToeRuby.Core.UnitTests/Validators/tc_tie_game_validation.rb
+++ TicTacToeRuby.Core.UnitTests/Validators/tc_tie_game_validation.rb
@@ -12,18 +12,32 @@ class TestTieGameValidation < Test::Unit::TestCase
     assert(!TieGameValidator.tie_game?(board), "Expected false to be returned on unplayed board.")
   end
 
-  def test_tie_game_happens_when_there_are_no_unoccupied_tiles_on_board
+  def test_tie_game_happens_when_there_are_no_unoccupied_tiles_on_board_with_no_win
     board = []
+    board << "Y" 
+    board << "X" 
+    board << "Y"
+    board << "X" 
+    board << "X" 
+    board << "Y"
+    board << "Y" 
+    board << "Y" 
     board << "X"
-    board << "X"
-    board << "X"
-    board << "X"
-    board << "Y"
-    board << "Y"
-    board << "Y"
-    board << "Y"
-    board << "Y"
     assert(TieGameValidator.tie_game?(board), "Expected detection of tie game.")
+  end
+
+  def test_tie_game_is_false_when_there_are_no_unoccupied_tiles_on_board_and_someone_won
+    board = []
+    board << "Y" 
+    board << "X" 
+    board << "Y"
+    board << "Y" 
+    board << "X" 
+    board << "Y"
+    board << "Y" 
+    board << "Y" 
+    board << "X"
+    assert(!TieGameValidator.tie_game?(board), "Since player Y won, it is not a tie game.")
   end
 
   def test_tie_game_returns_false_when_there_is_an_unoccupied_tile_on_board
