e7c23fe5e5baffde7db8f4c7ca532f98bd0b1524
ranizilpelwar <ranizilpelwar@gmail.com>
2018-03-23 16:06:27 -0400

    Consolidated board creation and instantiation into one line.

--- TicTacToeRuby.Core/GamePlay/game_board.rb
+++ TicTacToeRuby.Core/GamePlay/game_board.rb
@@ -11,16 +11,7 @@ class GameBoard
   end
 
   def self.create_board
-    board = []
-    board << "1"
-    board << "2"
-    board << "3"
-    board << "4"
-    board << "5"
-    board << "6"
-    board << "7"
-    board << "8"
-    board << "9"
+    board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
   end
 
   def update_board(index, player_symbol)
