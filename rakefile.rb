f45e7daa215b612d8c674a63faf2eea693c6887e
ranizilpelwar <ranizilpelwar@gmail.com>
2018-03-23 11:53:44 -0400

    Updated rakefile to include test from Core GamePlay folder

---
+++ rakefile.rb
@@ -0,0 +1,9 @@
+task :run_tests do
+  require_relative 'TicTacToeRuby.Core.UnitTests/Players/tc_player_type.rb'
+  ruby 'TicTacToeRuby.Core.UnitTests/Players/tc_player_type.rb'
+  ruby 'TicTacToeRuby.Core.UnitTests/Players/tc_player_manager_initialization.rb'
+  ruby 'TicTacToeRuby.Core.UnitTests/Players/tc_player_manager.rb'
+  ruby 'TicTacToeRuby.Core.UnitTests/Players/tc_player.rb'
+  ruby 'TicTacToeRuby.Core.UnitTests/Players/tc_player_movement_manager.rb'
+  ruby 'TicTacToeRuby.Core.UnitTests/GamePlay/tc_match_type.rb'
+end
