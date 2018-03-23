6256b3355a02aee9f98b4e3d2f6e1e8781d8ccee
ranizilpelwar <ranizilpelwar@gmail.com>
2018-03-23 12:02:12 -0400

    Added Core GamePlay tests to rakefile

--- rakefile.rb
+++ rakefile.rb
@@ -6,4 +6,7 @@ task :run_tests do
   ruby 'TicTacToeRuby.Core.UnitTests/Players/tc_player.rb'
   ruby 'TicTacToeRuby.Core.UnitTests/Players/tc_player_movement_manager.rb'
   ruby 'TicTacToeRuby.Core.UnitTests/GamePlay/tc_match_type.rb'
+  ruby 'TicTacToeRuby.Core.UnitTests/GamePlay/tc_match_type_manager.rb'
+  ruby 'TicTacToeRuby.Core.UnitTests/GamePlay/tc_game_board.rb'
+  ruby 'TicTacToeRuby.Core.UnitTests/GamePlay/tc_computer_actions.rb'
 end
