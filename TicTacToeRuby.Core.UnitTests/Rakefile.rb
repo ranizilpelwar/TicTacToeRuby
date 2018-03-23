34ba579ef256d26bc322e0b531d35bec2e18174a
ranizilpelwar <ranizilpelwar@gmail.com>
2018-03-23 11:50:39 -0400

    Moved rakefile to top level folder

--- TicTacToeRuby.Core.UnitTests/Rakefile.rb
+++
@@ -1,7 +0,0 @@
-task :run_tests do
-  require_relative 'Players/tc_player_type.rb'
-  ruby 'Players/tc_player_type.rb'
-  ruby 'Players/tc_player_manager_initialization.rb'
-  ruby 'Players/tc_player_manager.rb'
-  ruby 'Players/tc_player.rb'
-end
