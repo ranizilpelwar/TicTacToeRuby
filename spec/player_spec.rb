4b312e67853ed6fbbcd8e772db47bcdaa290cd97
ranizilpelwar <ranizilpelwar@gmail.com>
2018-03-26 09:20:11 -0400

    Added more scenarios

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,25 +2,50 @@ require_relative '../TicTacToeRuby.Core/Players/player.rb'
 require_relative '../TicTacToeRuby.Core/Players/player_type.rb'
 
 RSpec.describe "a player" do
-  
-  def player(type, symbol)
-  	Player.new(type, symbol)
-  end
 
 	it "has a symbol" do
-		raise unless player("Human", "X").symbol == "X" 
+		raise if player("Human", "X").symbol != "X" 
 	end
+
+  describe "symbol" do
+    it "can have alpha characters" do
+      raise if !symbol_valid?("a")
+    end
+    it "is one character long" do
+      raise if symbol_valid?("ab")
+    end
+    it "does not have numbers in it" do
+      raise if symbol_valid?("1")
+    end
+  end
+
 	it "has a type" do
-		raise unless player("Human", "X").type == "Human"
+		raise if player("Human", "X").type != "Human"
 	end
 
 	describe "type" do
 	  it "can be a computer" do
-	    raise unless PlayerType.new(:Computer).selected_option == :Computer 
+	    raise if player_type_option(:Computer) != :Computer 
 	  end
 	  it "can be a human" do
-	    raise unless PlayerType.new(:Human).selected_option == :Human 
+	    raise if player_type_option(:Human) != :Human 
 	  end
+  end
+
+
+
+# Automation Logic
+  
+  def player(type, symbol)
+    Player.new(type, symbol)
+  end
+
+  def symbol_valid?(symbol)
+    PlayerSymbolValidator.valid?(symbol)
+  end
+
+  def player_type_option(type)
+    PlayerType.new(type).selected_option
   end
 end
 
