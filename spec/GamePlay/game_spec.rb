20553b594e777d30f182381d5988077a79d6fd1a
Rani Zilpelwar <rani@8thlight.com>
2018-03-26 16:46:06 -0400

    Fix formatting

--- spec/GamePlay/game_spec.rb
+++ spec/GamePlay/game_spec.rb
@@ -67,31 +67,31 @@ RSpec.describe "a game" do
     raise if manager.get_last_move_for_player(player_number) != updated_move
   end
 
-def writer_double
-  double("ConsoleWriter", :display_message => "")
-end
+  def writer_double
+    double("ConsoleWriter", :display_message => "")
+  end
 
-def reader_double
-  double("ConsoleReader", :read_input => "1", :read_input_ignore_empty => "1")
-end
+  def reader_double
+    double("ConsoleReader", :read_input => "1", :read_input_ignore_empty => "1")
+  end
 
-def match_type_manager_double
-  player1_type = double("PlayerType", :selected_option => :Human)
-  player2_type = double("PlayerType", :selected_option => :Human)
-  match = double("MatchType", :player1_type => player1_type, :player2_type => player2_type)
-  player_type = double("PlayerType", )
-  match_manager = double("MatchTypeManager", :get_total_available_matches => "1", :matches => [match], :valid? => true, :get_match_type => match)
-end
+  def match_type_manager_double
+    player1_type = double("PlayerType", :selected_option => :Human)
+    player2_type = double("PlayerType", :selected_option => :Human)
+    match = double("MatchType", :player1_type => player1_type, :player2_type => player2_type)
+    player_type = double("PlayerType", )
+    match_manager = double("MatchTypeManager", :get_total_available_matches => "1", :matches => [match], :valid? => true, :get_match_type => match)
+  end
 
-def match_manager
-  MatchTypeManager.new
-end
+  def match_manager
+    MatchTypeManager.new
+  end
 
-def match_type(player1_type, player2_type)
-  match = double("MatchType", :player1_type => player1_type, :player2_type => player2_type)
-end
+  def match_type(player1_type, player2_type)
+    match = double("MatchType", :player1_type => player1_type, :player2_type => player2_type)
+  end
 
-def movement_manager(match_type)
-  PlayerMovementManager.new(match_type)
+  def movement_manager(match_type)
+    PlayerMovementManager.new(match_type)
 end
 end
