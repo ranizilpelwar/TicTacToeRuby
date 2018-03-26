7f313cb0ad65447caee937f51adcdaf304918bcf
ranizilpelwar <ranizilpelwar@gmail.com>
2018-03-26 13:27:42 -0400

    preliminary specs for a match

---
+++ spec/GamePlay/match_spec.rb
@@ -0,0 +1,19 @@
+require_relative '../../TicTacToeRuby.Core/GamePlay/match_type_manager.rb'
+
+RSpec.describe "a match" do
+  
+  it "is against two players"
+
+  it "can be a Human vs a Human"
+
+  it "can be a Human vs a Computer"
+
+  it "can be a Computer vs a Computer"
+  
+end
+
+RSpec.describe "a game" do 
+
+  it "has three matches available to play"
+
+end
