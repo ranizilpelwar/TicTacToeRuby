9c1943977882b1d5fa5d513dae0dd1a830c9e032
Rani Zilpelwar <rani@8thlight.com>
2018-03-27 16:22:38 -0400

    Added tests from unit test file

--- spec/Validators/available_spaces_validator_spec.rb
+++ spec/Validators/available_spaces_validator_spec.rb
@@ -6,6 +6,38 @@ RSpec.describe "an available spaces validator" do
     it "raises an ArgumentError when the board is nil" do
       expect{ AvailableSpacesValidator.get_available_spaces(nil) }.to raise_error(ArgumentError)
     end
+
+    it "returns one element when there is only one available spot on the board" do
+      board = ["X", "X", "X", "X", "X", "X", "X", "X", "9"]
+      expected_result = 1
+      available_spaces = AvailableSpacesValidator.get_available_spaces(board)
+      actual_result = available_spaces.length
+      expect(actual_result).to eq(expected_result)
+    end
+
+    it "returns zero elements when there are no available spots on the board" do
+      board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
+      expected_result = 0
+      available_spaces = AvailableSpacesValidator.get_available_spaces(board)
+      actual_result = available_spaces.length
+      expect(actual_result).to eq(expected_result)
+    end
+
+    it "returns nine elements when they are all available spots on the board" do
+      board = GameBoard.create_board
+      expected_result = 9
+      available_spaces = AvailableSpacesValidator.get_available_spaces(board)
+      actual_result = available_spaces.length
+      expect(actual_result).to eq(expected_result)
+    end
+
+    it "contains expected index when there is only one available spot on the board" do
+      board = ["X", "X", "X", "X", "X", "X", "X", "X", "9"]
+      expected_result = 8
+      available_spaces = AvailableSpacesValidator.get_available_spaces(board)
+      actual_result = available_spaces[0]
+      expect(actual_result).to eq(expected_result)
+    end
   end
 
   shared_examples "digit?" do |description, value, expected|
@@ -23,5 +55,4 @@ RSpec.describe "an available spaces validator" do
     include_examples "digit?", "a zero", "0", true
     include_examples "digit?", "a negative number", "-1", false
   end
-
 end
