720cf9312d99db4b6413f356cd16a54159dde525
Rani Zilpelwar <rani@8thlight.com>
2018-03-27 16:38:24 -0400

    Added parameterized test template

--- spec/Validators/board_score_validator_spec.rb
+++ spec/Validators/board_score_validator_spec.rb
@@ -18,6 +18,13 @@ RSpec.describe "a board score validator" do
     end
   end
 
+  shared_examples "score of line" do |scenario, board, indexes, expected_score|
+    it "returns expected score for scenario #{scenario}" do
+      actual_score = BoardScoreValidator.evaluate_score_of_line(board, indexes, "O", "X")
+      expect(actual_score).to eq(expected_score)
+    end
+  end
+
   context "method called evaluate_score_of_line" do
     it "raises an ArgumentError when board is nil" do
       expect{ BoardScoreValidator.evaluate_score_of_line(nil, [], "X", "Y") }.to raise_error(ArgumentError)
@@ -37,5 +44,10 @@ RSpec.describe "a board score validator" do
       board = GameBoard.create_board
       expect{ BoardScoreValidator.evaluate_score_of_line(board, [], "X", "1") }.to raise_error(ArgumentError)
     end
+
+    include_examples "score of line", "1", ["O", "O", "O", "O", "5", "O", "7", "O", "X"], [0, 1, 2], 100
+    
   end
+
+
 end
