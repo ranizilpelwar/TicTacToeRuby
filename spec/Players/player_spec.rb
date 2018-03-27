2af91fe269e35ef5ddb2b3f2db172ca81a9a84be
Rani Zilpelwar <rani@8thlight.com>
2018-03-27 09:41:53 -0400

    Use expect syntax

--- spec/Players/player_spec.rb
+++ spec/Players/player_spec.rb
@@ -4,70 +4,81 @@ require_relative '../../TicTacToeRuby.Core/Validators/player_symbol_validator.rb
 
 RSpec.describe "a player" do
 
+
 	it "has a symbol" do
-		raise if player(:Human, "X").symbol != "X" 
+		expect(player(:Human, "X").symbol).to eq("X") 
 	end
 
   describe "symbol" do
     it "is one character long" do
-      raise if symbol_valid?("ab")
+      expect(symbol_valid?("a")).to be true
+    end
+
+    it "cannot be more than one character" do
+      expect(symbol_valid?("ab")).to be false
     end
     
     it "can be an alpha character" do
-      raise if !symbol_valid?("a")
+      expect(symbol_valid?("a")).to be true
     end
 
     it "can be a keyboard symbol" do
-      raise if !symbol_valid?("$")
+      expect(symbol_valid?("$")).to be true
     end
 
     it "cannot be a number" do
-      raise if symbol_valid?("1")
+      expect(symbol_valid?("1")).to be false
     end
 
     it "cannot be a space character" do
-      raise if symbol_valid?(" ")
+      expect(symbol_valid?(" ")).to be false
     end
 
     it "cannot be an empty character" do
-      raise if symbol_valid?("")
+      expect(symbol_valid?("")).to be false
     end
   end
 
 	it "has a type" do
-		raise if player(:Human, "X").type != :Human
+		expect(player(:Human, "X").type).to eq(:Human)
 	end
 
 	describe "type" do
 	  it "can be a computer" do
-	    raise if player_type(:Computer).selected_option != :Computer 
+	    expect(player_type(:Computer).selected_option).to eq(:Computer) 
 	  end
 
 	  it "can be a human" do
-	    raise if player_type(:Human).selected_option != :Human 
+	    expect(player_type(:Human).selected_option).to eq(:Human)
 	  end
 
     it "cannot be a robot" do
-      raise if PlayerType.valid?(:Robot)
+      expect(PlayerType.valid?(:Robot)).to be false
     end
   end
 
   describe "two players" do
     context "with the same type and symbol" do
       it "are equal" do
-        raise if !player(:Human, "X").equals?(player(:Human, "X"))
+        expect(player(:Human, "X").equals?(player(:Human, "X"))).to be true
       end
     end
 
     context "with the same type and a different symbol" do
       it "are not equal" do
-        raise if player(:Human, "X").equals?(player(:Human, "Y"))
+        expect(player(:Human, "X").equals?(player(:Human, "Y"))).to be false
       end
     end
 
     context "with a different type and same symbol" do
       it "are not equal" do
-        raise if player(:Human, "X").equals?(player(:Computer, "X"))
+        expect(player(:Human, "X").equals?(player(:Computer, "X"))).to be false
+      end
+    end
+
+    context "with a different type and different symbol" do
+      it "are not equal" do
+        expect(player(:Human, "X").equals?(player(:Computer, "Y"))).to be false
       end
     end
   end
