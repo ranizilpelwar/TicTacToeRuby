aa261f5f4c26d9d3601889c1ae59ec934c508449
Rani Zilpelwar <rani@8thlight.com>
2018-03-27 09:11:40 -0400

    Refactored to use expect syntax

--- spec/GamePlay/game_spec.rb
+++ spec/GamePlay/game_spec.rb
@@ -9,7 +9,7 @@ require_relative '../../TicTacToeRuby.Console/GamePlay/match_type_setup.rb'
 RSpec.describe "a game" do 
 
   it "has three matches available to play" do
-    raise if match_manager.get_total_available_matches != 3
+    expect(match_manager.get_total_available_matches).to eq(3)
   end
 
   it "allows the user to chose the game type" do
@@ -17,32 +17,32 @@ RSpec.describe "a game" do
     reader = reader_double
     match_type_manager = match_type_manager_double
     match_type = MatchTypeSetup.get_valid_match_type(writer, reader, match_type_manager)
-    raise if match_type.nil?
+    expect(match_type).to_not be_nil
   end
 
   it "is against two players" do
     player1 = double()
     player2 = double()
     match = match_type(player1, player2)
-    raise if match.player1_type.nil?
-    raise if match.player2_type.nil?
+    expect(match.player1_type).to_not be_nil
+    expect(match.player2_type).to_not be_nil
   end
 
   it "allows the user to choose which player goes first" do
     symbol1 = "X"
     symbol2 = "Y"
     selection = "X"
-    raise if !PlayerSelectionValidator.valid?(selection, symbol1, symbol2)
+    expect(PlayerSelectionValidator.valid?(selection, symbol1, symbol2)).to be true
   end
 
   it "can end in a win when a player has three squares in a row" do
     board = ["X", "X", "X", "4", "5", "6", "7", "8", "9"]
-    raise if !GameOverValidator.game_over?(board)
+    expect(GameOverValidator.game_over?(board)).to be true
   end
 
   it "can end in a win when a player has three squares in a column" do
     board = ["X", "2", "3", "X", "5", "6", "X", "8", "9"]
-    raise if !GameOverValidator.game_over?(board)
+    expect(GameOverValidator.game_over?(board)).to be true
   end
 
   it "can end in a win when a player has three squares diagonally" do
@@ -50,12 +50,13 @@ RSpec.describe "a game" do
     win_left_diagonal = GameOverValidator.game_over?(board1)
     board2 = ["1", "2", "X", "4", "X", "6", "X", "8", "9"]
     win_right_diagonal = GameOverValidator.game_over?(board2)
-    raise if !win_left_diagonal && !win_right_diagonal
-  end
+    expect(win_left_diagonal).to be true
+    expect(win_right_diagonal).to be true
+  end 
 
   it "can end in a tie when neither player gets three squares consecutively on a board" do
     board = ["X", "X", "Y", "Y", "Y", "X", "X", "X", "Y"]
-    raise if !TieGameValidator.tie_game?(board)
+    expect(TieGameValidator.tie_game?(board)).to be true
   end
 
   it "can store a player's last move" do
@@ -64,7 +65,7 @@ RSpec.describe "a game" do
     updated_move = 4
     manager = movement_manager(match_type)
     stored_value = manager.update_last_move_for_player(player_number, updated_move)
-    raise if manager.get_last_move_for_player(player_number) != updated_move
+    expect(manager.get_last_move_for_player(player_number)).to eq(updated_move)
   end
 
   def writer_double
