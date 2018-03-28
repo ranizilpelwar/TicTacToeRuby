8ed00dc0fdde309bb659323e0e6f327909ba9b62
Rani Zilpelwar <rani@8thlight.com>
2018-03-28 10:07:50 -0400

    Added all the unit tests

--- spec/Validators/game_play_validator_spec.rb
+++ spec/Validators/game_play_validator_spec.rb
@@ -3,10 +3,92 @@ require_relative '../../TicTacToeRuby.Core/GamePlay/game_board.rb'
 require_relative '../../TicTacToeRuby.Core.UnitTests/Players/mock_player_manager.rb'
 
 RSpec.describe "a game play validator" do
+  
+  before(:example) do
+    player_manager = MockPlayerManager.new
+    board = [ "0", "1", "2",
+              "3", "X", "5",
+              "O", "X", "O" ]
+    @game_board = GameBoard.new(player_manager, board)
+  end
+  
+  shared_examples "evaluate_move" do |description, value, expected|
+    it "returns #{expected} when tile is #{description}" do
+      expect(GamePlayValidator.evaluate_move(@game_board, value).is_valid_move).to be expected
+    end
+  end
+
   context "method called evaluate_move" do
     it "raises an ArgumentError when game board is nil" do
       expect{ GamePlayValidator.evaluate_move(nil, "1") }.to raise_error(ArgumentError)
     end
 
+    it "returns two elements" do
+      result = GamePlayValidator.evaluate_move(@game_board, "1")
+      expected_length = 2
+      expect(result.length).to eq(expected_length)
+    end
+
+    include_examples "evaluate_move", "occupied", "4", false
+    include_examples "evaluate_move", "an alpha character", "X", false
+    include_examples "evaluate_move", "an out-of-bounds value", "10", false
+    include_examples "evaluate_move", "unoccupied", "0", true
+
+    it "returns_correct_index_when_tile_is_unoccupied" do
+      expect(GamePlayValidator.evaluate_move(@game_board, "0").index_of_board).to eq(0)
+    end
+  end
+
+  context "method called winning_game?" do
+    it "raises an ArgumentError when game board is nil" do
+      available_spaces = [0, 1]
+      expect{ GamePlayValidator.winning_game?("X", nil, available_spaces) }.to raise_error(ArgumentError)
+    end
+
+    it "raises an ArgumentError when available spaces is nil" do
+      expect{ GamePlayValidator.winning_game?("X", @game_board.board, nil) }.to raise_error(ArgumentError)
+    end
+
+    it "raises an ArgumentError when player symbol is invalid" do
+      available_spaces = [0, 1]
+      expect{ GamePlayValidator.winning_game?("1", @game_board.board, available_spaces) }.to raise_error(ArgumentError)
+    end
+
+    it "returns two elements" do
+      available_spaces = [0, 1, 2, 3, 5]
+      result = GamePlayValidator.winning_game?("X", @game_board.board, available_spaces)
+      expected_length = 2
+      expect(result.length).to eq(expected_length)
+    end
+
+    it "returns true when winning move can be made" do
+      available_spaces = [0, 1]
+      result = GamePlayValidator.winning_game?("X", @game_board.board, available_spaces)
+      expect(result.is_valid_move).to be true
+    end
+
+    it "returns correct index when winning move can be made" do
+      available_spaces = [0, 1]
+      result = GamePlayValidator.winning_game?("X", @game_board.board, available_spaces)
+      expect(result.index_of_board).to eq(1)
+    end
+
+    it "returns false when winning move cannot be made" do
+      available_spaces = [0, 5]
+      result = GamePlayValidator.winning_game?("X", @game_board.board, available_spaces)
+      expect(result.is_valid_move).to be false
+    end
+
+    it "returns negative one when winning move cannot be made" do
+      available_spaces = [0, 5]
+      result = GamePlayValidator.winning_game?("X", @game_board.board, available_spaces)
+      expect(result.index_of_board).to eq(-1)
+    end
+
+    it "returns false when available spaces is empty" do
+      available_spaces = []
+      result = GamePlayValidator.winning_game?("X", @game_board.board, available_spaces)
+      expect(result.is_valid_move).to be false
+    end
   end
 end
