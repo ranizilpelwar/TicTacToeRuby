84dc2efd3ddf00bccaccaea20206ddf92ba92f05
Rani Zilpelwar <rani@8thlight.com>
2018-03-28 10:24:35 -0400

    Add all unit tests for player symbol validator

---
+++ spec/Validators/player_symbol_validator_spec.rb
@@ -0,0 +1,21 @@
+require_relative '../../TicTacToeRuby.Core/Validators/player_symbol_validator.rb'
+
+RSpec.describe "a player symbol validator" do
+  shared_examples "valid symbol" do |description, input, expected|
+    it "returns #{expected} for #{description}" do
+      expect(PlayerSymbolValidator.valid?(input)).to be expected
+    end
+  end
+
+  context "method called valid?" do
+    include_examples "valid symbol", "an empty string", "", false
+    include_examples "valid symbol", "a single digit", "1", false
+    include_examples "valid symbol", "a dual digit", "11", false
+    include_examples "valid symbol", "an input with length greater than 1", "XX", false
+    include_examples "valid symbol", "an upper case alpha character", "A", true
+    include_examples "valid symbol", "a lower case alpha character", "a", true
+    include_examples "valid symbol", "a percent character", "%", true
+    include_examples "valid symbol", "a pound character", "#", true
+    include_examples "valid symbol", "a space character", " ", false
+  end
+end
