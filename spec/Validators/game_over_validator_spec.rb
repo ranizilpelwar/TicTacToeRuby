857532eb31df8ea0c3026664b7af68278138f5fc
Rani Zilpelwar <rani@8thlight.com>
2018-03-28 09:34:25 -0400

    Added tests for win_for_player method

--- spec/Validators/game_over_validator_spec.rb
+++ spec/Validators/game_over_validator_spec.rb
@@ -26,5 +26,30 @@ RSpec.describe "a game over validator" do
     include_examples "game_over?", "there are no played moves on the board yet", GameBoard.create_board, false
   end
 
-  
+  shared_examples "win_for_player?" do |description, symbol, board, expected|
+    it "returns #{expected} when #{description}" do
+      expect(GameOverValidator.win_for_player?(symbol, board)).to be expected
+    end
+  end
+
+  context "method called win_for_player?" do
+    it "raises an ArgumentError when board is nil" do
+      expect{ GameOverValidator.win_for_player?("X", nil)}.to raise_error(ArgumentError)
+    end
+    
+    it "raises an ArgumentError when player symbol is invalid" do
+      expect{ GameOverValidator.win_for_player?("1", GameBoard.create_board)}.to raise_error(ArgumentError)
+    end
+
+    include_examples "win_for_player?", "first row contains matching tiles", "X", ["X", "X", "X", "X", "4", "5", "6", "7", "8", "9"], true
+    include_examples "win_for_player?", "middle row contains matching tiles", "X", ["1", "2", "3", "X", "X", "X", "7", "8", "9"], true
+    include_examples "win_for_player?", "last row contains matching tiles", "X", ["1", "2", "3", "4", "5", "6", "X", "X", "X"], true
+    include_examples "win_for_player?", "first column contains matching tiles", "X", ["X", "2", "3", "X", "5", "6", "X", "8", "9"], true
+    include_examples "win_for_player?", "middle column contains matching tiles", "Y", ["1", "Y", "3", "4", "Y", "6", "7", "Y", "9"], true
+    include_examples "win_for_player?", "last column contains matching tiles", "X", ["1", "2", "X", "4", "5", "X", "7", "8", "X"], true
+    include_examples "win_for_player?", "left to right diagonal contains matching tiles", "X", ["X", "2", "3", "4", "X", "6", "7", "8", "X"], true
+    include_examples "win_for_player?", "right to left diagonal contains matching tiles", "X", ["1", "2", "X", "4", "X", "6", "X", "8", "9"], true
+    include_examples "win_for_player?", "there are no matching tiles on the board", "X", ["1", "2", "3", "4", "X", "6", "X", "8", "9"], false
+    include_examples "win_for_player?", "there are no played moves on the board yet", "X", GameBoard.create_board, false
+  end
 end
