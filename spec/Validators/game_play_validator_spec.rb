e6d641ba1ee553a54fd4d1253c2b1cbcfce22a21
Rani Zilpelwar <rani@8thlight.com>
2018-03-28 09:38:34 -0400

    Adding tests for next validator

---
+++ spec/Validators/game_play_validator_spec.rb
@@ -0,0 +1,12 @@
+require_relative '../../TicTacToeRuby.Core/Validators/game_play_validator.rb'
+require_relative '../../TicTacToeRuby.Core/GamePlay/game_board.rb'
+require_relative '../Players/mock_player_manager.rb'
+
+RSpec.describe "a game play validator" do
+  context "method called evaluate_move" do
+    it "raises an ArgumentError when game board is nil" do
+      expect{ GamePlayValidator.evaluate_move(nil, "1") }.to raise_error(ArgumentError)
+    end
+
+  end
+end
