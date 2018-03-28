a2b90c97993880446a103305f3b6e6349dbccca5
Rani Zilpelwar <rani@8thlight.com>
2018-03-28 10:38:21 -0400

    Refactored board creation to be one line

--- TicTacToeRuby.Core.UnitTests/Validators/tc_tie_game_validation.rb
+++ TicTacToeRuby.Core.UnitTests/Validators/tc_tie_game_validation.rb
@@ -13,44 +13,17 @@ class TestTieGameValidation < Test::Unit::TestCase
   end
 
   def test_tie_game_happens_when_there_are_no_unoccupied_tiles_on_board_with_no_win
-    board = []
-    board << "Y" 
-    board << "X" 
-    board << "Y"
-    board << "X" 
-    board << "X" 
-    board << "Y"
-    board << "Y" 
-    board << "Y" 
-    board << "X"
+    board = ["Y", "X", "Y", "X", "X", "Y", "Y", "Y", "X"]
     assert(TieGameValidator.tie_game?(board), "Expected detection of tie game.")
   end
 
   def test_tie_game_is_false_when_there_are_no_unoccupied_tiles_on_board_and_someone_won
-    board = []
-    board << "Y" 
-    board << "X" 
-    board << "Y"
-    board << "Y" 
-    board << "X" 
-    board << "Y"
-    board << "Y" 
-    board << "Y" 
-    board << "X"
+    board = ["Y", "X", "Y", "Y", "X", "Y", "Y", "Y", "X"]
     assert(!TieGameValidator.tie_game?(board), "Since player Y won, it is not a tie game.")
   end
 
   def test_tie_game_returns_false_when_there_is_an_unoccupied_tile_on_board
-    board = []
-    board << "X"
-    board << "X"
-    board << "3"
-    board << "X"
-    board << "Y"
-    board << "Y"
-    board << "Y"
-    board << "Y"
-    board << "Y"
+    board = ["X", "X", "3", "X", "Y", "Y", "Y", "Y", "Y"]
     assert(!TieGameValidator.tie_game?(board), "Expected tie game to return false as there is one available tile on the board.")
   end
 end
