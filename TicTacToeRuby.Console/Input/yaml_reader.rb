6624e148c67e67bde9b3392c3c514602a7ce2db3
Rani Zilpelwar <rani@8thlight.com>
2018-03-29 13:41:59 -0400

    added variable to hold language setting which impacts which language file to use

--- TicTacToeRuby.Console/Input/yaml_reader.rb
+++ TicTacToeRuby.Console/Input/yaml_reader.rb
@@ -2,7 +2,18 @@ require 'yaml'
 
 module YAMLReader
 
-  def self.read_data(file_name, property)
+  @localization = "en"
+
+  def self.set_localization(language)
+    @localization = language
+  end
+
+  def self.generate_file_name(file)
+    file_name = "TicTacToeRuby.Console/Languages/" + file + "." + @localization.to_s + ".yaml"
+  end
+
+  def self.read_data(file, property)
+    file_name = generate_file_name(file)
     file = YAML.load_file(file_name)
     data = file[property]
   end
