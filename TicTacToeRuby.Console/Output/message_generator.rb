f3ffa9ac5dc8fd5e9cb13122bd5d9f6cb141e7ab
Rani Zilpelwar <rani@8thlight.com>
2018-03-29 13:41:26 -0400

    using this module to handle interaction with yaml reader

---
+++ TicTacToeRuby.Console/Output/message_generator.rb
@@ -0,0 +1,12 @@
+require_relative '../Input/yaml_reader.rb'
+
+module MessageGenerator
+
+  def self.set_localization(language)
+      YAMLReader.set_localization(language)
+  end
+
+  def self.welcome
+    message = YAMLReader.read_data("application_text", "welcome_message")
+  end
+end
