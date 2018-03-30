bb499131a909ad303930945b1e940dbd59ef2eb3
Rani Zilpelwar <rani@8thlight.com>
2018-03-30 10:46:58 -0400

    Consolidated base case and refactored code toremove comments

--- TicTacToeRuby.Core/GamePlay/computer_actions.rb
+++ TicTacToeRuby.Core/GamePlay/computer_actions.rb
@@ -17,6 +17,10 @@ class ComputerActions
     @other_player = @player_manager.get_next_player.symbol
   end
 
+  def reset_board_tile(board, move, tile)
+    board[move] = tile
+  end
+
   def get_best_move(board, player_symbol, depth, best_max_value, best_min_value)
     raise ArgumentError, "Cannot get best move because board is nil." if board.nil?
     raise ArgumentError, "Cannot get best move because player_symbol is invalid." if !PlayerSymbolValidator.valid?(player_symbol)
@@ -24,56 +28,51 @@ class ComputerActions
     tile = ""
     current_score = 0
     best_move = -1
-    if next_moves.size == 0 || depth == 0
-      current_score = BoardScoreEvaluator.score_of_board(board, current_player, other_player)
-      move = WeightedMove.new(best_move, current_score)
-      #return move
+
+    current = player_symbol == current_player
+    if current 
+        opposing_symbol = other_player
     else
-      current = player_symbol == current_player
-      if current 
-          opposing_symbol = other_player
-      else
-          opposing_symbol = current_player
-      end
-      next_moves.each do |move|
-	      tile = board[move]
-        board[move] = player_symbol
-        win_exists = GameOverValidator.win_for_player?(player_symbol, board)
-        
-    	  if win_exists
-          # An early win or block has an advantage:
-    	    current_score = BoardScoreEvaluator.score_of_board(board, current_player, other_player) * depth
-    	  else
-    	    current_score = get_best_move(board, opposing_symbol, depth - 1, best_max_value, best_min_value).score
-        end
-        if current
-          if current_score > best_max_value
-            best_max_value = current_score
-            best_move = move
-      	  end
-        else
-          if current_score < best_min_value
-            best_min_value = current_score
-            best_move = move
-          end
-      	end
-    	  #Reset the board tile:
-        board[move] = tile
-    	  #Stop traversing more nodes in this branch:
-    	  break if best_max_value >= best_min_value
-      end
-      if current
-        score = best_max_value
-      else
-        score = best_min_value
-      end
-      weighted_move = WeightedMove.new(best_move, score)
-      #return weighted_move
+        opposing_symbol = current_player
     end
 
-
-
+    win_exists = GameOverValidator.win_for_player?(player_symbol, board) || GameOverValidator.win_for_player?(opposing_symbol, board)
     
+    if next_moves.size == 0 || depth == 0 || win_exists
+      current_score = BoardScoreEvaluator.score_of_board(board, current_player, other_player) * (depth + 1)
+      move = WeightedMove.new(best_move, current_score)
+      return move
+    end
+
+    next_moves.each do |move|
+      tile = board[move]
+      board[move] = player_symbol
+	    current_score = get_best_move(board, opposing_symbol, depth - 1, best_max_value, best_min_value).score
+      if current
+        if current_score > best_max_value
+          best_max_value = current_score
+          best_move = move
+    	  end
+      else
+        if current_score < best_min_value
+          best_min_value = current_score
+          best_move = move
+        end
+    	end
+
+      reset_board_tile(board, move, tile)
+      found_best_route = best_max_value >= best_min_value
+
+  	  break if found_best_route
+    end
+    if current
+      score = best_max_value
+    else
+      score = best_min_value
+    end
+
+    weighted_move = WeightedMove.new(best_move, score)
+    return weighted_move
   end
 end
 
