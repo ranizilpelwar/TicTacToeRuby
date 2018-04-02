27affd7850933043744ee55717884260d4c4d183
Rani Zilpelwar <rani@8thlight.com>
2018-04-02 09:18:05 -0400

    Added error handling

--- TicTacToeRuby.Console/Input/yaml_reader.rb
+++ TicTacToeRuby.Console/Input/yaml_reader.rb
@@ -4,10 +4,16 @@ require_relative '../Languages/language_setup.en.rb'
 module YAMLReader
 
   def self.generate_file_name(file)
+    raise ArgumentError, "YAMLReader cannot generate file name as file parameter is nil." if file.nil?
+    raise ArgumentError, "YAMLReader cannot generate file name as file parameter is empty." if file == ""
     file_name = "TicTacToeRuby.Console/Languages/" + file + "." + LanguageSetup.get_localization + ".yaml"
   end
 
   def self.read_data(file, property)
+    raise ArgumentError, "YAMLReader cannot generate file name as file parameter is nil." if file.nil?
+    raise ArgumentError, "YAMLReader cannot generate file name as property parameter is nil." if property.nil?
+    raise ArgumentError, "YAMLReader cannot generate file name as file parameter is empty." if file == ""
+    raise ArgumentError, "YAMLReader cannot generate file name as property parameter is empty." if property == ""
     file_name = generate_file_name(file)
     file = YAML.load_file(file_name)
     data = file[property]
