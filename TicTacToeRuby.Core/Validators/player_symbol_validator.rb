d9d2c9fed81a4f0e94c3b11204d4f13dd99af836
Rani Zilpelwar <rani@8thlight.com>
2018-04-03 14:33:26 -0400

    Refactored this class to use MessageGenerator

--- TicTacToeRuby.Core/Validators/player_symbol_validator.rb
+++ TicTacToeRuby.Core/Validators/player_symbol_validator.rb
@@ -1,5 +1,7 @@
 module PlayerSymbolValidator
   def self.valid?(input)
+    raise ArgumentError, MessageGenerator.argument_error("determine if valid", "input", "nil") if input.nil?
+    raise ArgumentError, MessageGenerator.argument_error("determine if valid", "input", "empty") if input == ""
     pattern = /\A[^0-9\s]{1}\z/
     match_data = pattern.match(input)
     match_data != nil
--- TicTacToeRuby.Core/Validators/tie_game_validator.rb
+++ TicTacToeRuby.Core/Validators/tie_game_validator.rb
@@ -3,7 +3,7 @@ require_relative 'game_over_validator.rb'
 
 module TieGameValidator
   def self.tie_game?(board)
-    raise ArgumentError, "Cannot determine if game is tied because provided board is nil." if board.nil?
+    raise ArgumentError, MessageGenerator.argument_error("determine if tie_game", "board", "nil") if board.nil?
     available_spaces = AvailableSpacesValidator.get_available_spaces(board)
     result = available_spaces.length == 0 && !GameOverValidator.game_over?(board)
   end
