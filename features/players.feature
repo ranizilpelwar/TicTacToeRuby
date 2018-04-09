74cd07224e3acc5f21868e64d69b4df9ab7c9ada
Rani Zilpelwar <rani@8thlight.com>
2018-04-09 11:03:46 -0400

    Added scenario for player

--- features/players.feature
+++ features/players.feature
@@ -1,22 +1,11 @@
-Feature: Setting up players for the game
-	There should only be two players in a game
-	A player can be a human or a computer	
-  A player cannot be anything else
+Feature: Setting up players for a game
 
-Scenario: A game can be played with one person
-Scenario: A game can be played with two people 
-	Scenario: A game can be played with no one
-
-Scenario: A game can not be played by a dog
-Given there are <number> persons who wants to play
-When the <number> persons play the game
-Then the <first> player is a <type1>
-And the <second> player is a <type2> 
-
-	Scenario: Someone wants to play a game
-		Given there is a <type>
-		When the <type> wants to play
-		Then they <expectation>
-		A game can be played by a human
-		A game can be played by a computer
-		A game can be played by a dog
+Scenario Outline: A player can be a human or a computer
+  When a <type> wants to play the game
+  Then they <expectation>
+		
+	Examples: 
+			|type    |expectation|
+			|human   |can        |
+			|computer|can        |
+			|dog     |cannot     |
--- features/step_definitions/players_steps.rb
+++ features/step_definitions/players_steps.rb
@@ -3,48 +3,17 @@ require_relative '../../TicTacToeRuby.Core/Players/player.rb'
 require_relative '../../TicTacToeRuby.Core/GamePlay/match_type.rb'
 require_relative '../../TicTacToeRuby.Core/GamePlay/match_type_manager.rb'
 
-Given("there is a person who wants to play") do
-  @type1 = PlayerType.new(:Human)
+When("a human wants to play the game") do
+  @type = :Human
+  @player_type = PlayerType.new(@type)
 end
 
-When("the person plays the game") do
-  @player1 = Player.new(@type1, "X")
-  @match_manager = MatchTypeManager.new	
+Then("they can") do
+  @player = Player.new(@player_type, "X")
+  expect(@player_type.selected_option).to eq(@type)
 end
 
-Then("the second player is a computer") do
-	expect(@match_manager.get_match_type(2).player2_type.selected_option).to eq(:Computer)
+When("a computer wants to play the game") do
+  @type = :Computer
+  @player_type = PlayerType.new(@type)
 end
-
-Given("there are two people that want to play") do
-	@type1 = PlayerType.new(:Human)
-	@type2 = PlayerType.new(:Human)
-end
-
-When("they play the game") do
-  @player1 = Player.new(@type1, "X")
-	@player2 = Player.new(@type2, "Y")
-	@match_manager = MatchTypeManager.new
-end
-
-Then("they play against each other") do
-  expect(@match_manager.get_match_type(1).player1_type.selected_option).to eq(:Human)
-	expect(@match_manager.get_match_type(1).player2_type.selected_option).to eq(:Human)
-end
-
-Given("there is no human to join the game") do
-	@type1 = PlayerType.new(:Computer)
-	@type2 = PlayerType.new(:Computer)
-end
-
-When("the game is played") do
-	@player1 = Player.new(@type1, "X")
-	@player2 = Player.new(@type2, "Y")
-	@match_manager = MatchTypeManager.new
-end
-
-Then("both players are of type computer") do
-	expect(@match_manager.get_match_type(3).player1_type.selected_option).to eq(:Computer)
-	expect(@match_manager.get_match_type(3).player2_type.selected_option).to eq(:Computer)
-end
-
