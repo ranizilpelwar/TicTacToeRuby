972619385fff8958ae2912f74c790226e87a0612
Rani Zilpelwar <rani@8thlight.com>
2018-04-12 11:40:59 -0400

    Fix bug where second test run results in error due to the default being changed by the prior test run

--- features/step_definitions/language_options_steps.rb
+++ features/step_definitions/language_options_steps.rb
@@ -4,10 +4,12 @@ Given("default language is {string}") do |language|
   @directory_path_for_default_language = "../TicTacToeRuby/features/test_files/"
 	@directory_path_for_default_language = @directory_path_for_default_language + "default_language_" + language.downcase + "/"
   raise_error(InvalidValueError) if !File.exist?(@directory_path_for_default_language)
+  @starting_default_tag = language == "English" ? "en" : "es"
 end
 
 When("the user is on the language configuration screen") do
 	@language_adapter_with_default_language_selected = LanguageOptionsAdapter.new(@directory_path_for_default_language)
+	@language_adapter_with_default_language_selected.set_default_language_tag(@starting_default_tag)
 end
 
 Then("the user is displayed the language choices as {string} and {string}") do |language1, language2|
@@ -24,7 +26,7 @@ end
 
 When("the user selects {string} as a language option") do |string|
   tag = @language_adapter_with_default_language_selected.get_language_tag_for_description(string)
-  @language_adapter_with_default_language_selected.set_default_language_tag(tag)
+	@language_adapter_with_default_language_selected.set_default_language_tag(tag)
 end
 
 Then("the default language stored in global settings is updated to {string}") do |string| 
