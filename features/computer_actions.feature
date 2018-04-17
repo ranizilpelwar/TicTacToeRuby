1a51c652f2f383b88f59c2aea380c3b3ca24c274
Rani Zilpelwar <rani@8thlight.com>
2018-04-17 09:03:46 -0400

    Add test for last move possible

--- features/computer_actions.feature
+++ features/computer_actions.feature
@@ -11,5 +11,11 @@ Feature: The game calculates the player's next move when the player is a Compute
 		When the computer player makes a move
 		Then the computer's next move matches square 5 on the board
 
+	Scenario: The Computer player selects the correct square on the board when there is one move left to play
+		Given a game is being played with a computer player
+		And selecting square 5 is the only spot left
+		When the computer player makes a move
+		Then the computer's next move matches square 5 on the board
+
 
 
--- features/step_definitions/computer_actions_steps.rb
+++ features/step_definitions/computer_actions_steps.rb
@@ -1,15 +1,15 @@
 Given("a game is being played with a computer player") do
   @player_manager = MockPlayerManager.new
 	@computer_player_symbol = "X"
+  @best_max_move = -20000
+	@best_min_move = 20000
+	@depth = 5
 end
 
 Given("selecting square 5 can result in a win for the computer player") do
   @new_board = [ "X", "O", "X", "X", "5", "O", "O", "O", "X" ]
   @game_board = GameBoard.new(@player_manager, @new_board)
 	@computer_actions = ComputerActions.new(@game_board)
-  @best_max_move = -20000
-	@best_min_move = 20000
-	@depth = 5
 end
 
 When("the computer player makes a move") do
@@ -19,4 +19,10 @@ end
 Then("the computer's next move matches square {int} on the board") do |int|
   index = int - 1 
 	expect(@actual_computer_move.index).to eq(index)
+end
+
+Given("selecting square {int} is the only spot left") do |int|
+	@new_board = [ "X", "O", "X", "X", "5", "O", "O", "X", "X" ]
+  @game_board = GameBoard.new(@player_manager, @new_board)
+	@computer_actions = ComputerActions.new(@game_board)
 end
