d75b460692d29afd6c679b1e6fa749475974dc70
Rani Zilpelwar <rani@8thlight.com>
2018-04-23 15:02:38 -0400

    Update test for methods in setup class. Adapter tests are already in the adapter class

--- spec/Console/language_setup_spec.rb
+++ spec/Console/language_setup_spec.rb
@@ -1,20 +1,14 @@
 require_relative '../../TicTacToeRuby.Console/Languages/language_options_adapter.rb'
 
-RSpec.describe "a language options adapter" do
+RSpec.describe "a language setup" do
   context "method called input_choices" do
-    it "raises an ArgumentError when options is nil" do
-      expect{ LanguageOptionsAdapter.new("TicTacToeRuby.Console/Languages/").input_choices(nil) }.to raise_error(ArgumentError)
-    end
-
     it "returns the expected list of input choices" do
-      expected_choices = ["1", "2"]
-      expect(LanguageOptionsAdapter.new("TicTacToeRuby.Console/Languages/").input_choices).to eq(expected_choices)
-    end
-  end
-
-  context "method called language_tags" do
-    it "returns the expected number of language tags" do
-      expect(LanguageOptionsAdapter.new("TicTacToeRuby.Console/Languages/").language_tags.size).to eq(2)
+      expected_choices = ["L", "l"]
+      args = {writer: double(), 
+            reader: double(), 
+            language_config: double(:valid => true)}
+      @language_setup = LanguageSetup.new(args)
+      expect(@language_setup.input_choices).to eq(expected_choices)
     end
   end
 end
