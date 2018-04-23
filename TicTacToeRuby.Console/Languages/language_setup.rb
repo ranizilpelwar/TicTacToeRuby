deec1fce50da44b4943dffb4e23918e913602321
Rani Zilpelwar <rani@8thlight.com>
2018-04-23 11:47:57 -0400

    add nil checks and modify variable names

--- TicTacToeRuby.Console/Languages/language_setup.rb
+++ TicTacToeRuby.Console/Languages/language_setup.rb
@@ -7,22 +7,28 @@ require_relative '../Output/yaml_writer.rb'
 
 class LanguageSetup
 
-  attr_reader :ui_writer, :ui_reader, :language_config
+  attr_reader :reader, :writer, :language_config
   
   def initialize(args)
-    @ui_writer = args[:writer]
-    @ui_reader = args[:reader]
+    @reader = args[:reader]
+    @writer = args[:writer]
     @language_config = args[:language_config]
+    raise NilReferenceError, "reader" if @reader.nil?
+    raise NilReferenceError, "writer" if @writer.nil?
+    raise NilReferenceError, "language_config" if @language_config.nil?
   end
 
-  def display_language_config_option
-    display_language_config_error if !@language_config.valid?(@language_config.stored_default_tag)
-    @ui_writer.display_message(MessageGenerator.language_configuration)
-    @ui_writer.display_message(MessageGenerator.line_spacer)
+  def display_prompt
+    @writer.display_message(MessageGenerator.language_defaults_error) if !@language_config.valid?(@language_config.stored_default_tag)
+    @writer.display_text(MessageGenerator.language_configuration_prompt)
   end
 
-  def display_language_config_error
-    @ui_writer.display_message(MessageGenerator.language_defaults_error)
+  def display_options
+    @writer.display_message(MessageGenerator.language_configuration_option)
+    @writer.display_message(MessageGenerator.line_spacer)
   end
 
+  def config_options
+    language_selections = ["L", "l"]
+  end
 end
