f3fd56068193a3a86718166d3262933bf6506c8e
Rani Zilpelwar <rani@8thlight.com>
2018-04-24 11:57:35 -0400

    Add method that allows usage of lambda so that it can be passed into the read and validate method as an input

--- TicTacToeRuby.Core/Validators/player_symbol_validator.rb
+++ TicTacToeRuby.Core/Validators/player_symbol_validator.rb
@@ -4,10 +4,16 @@ require_relative '../../TicTacToeRuby.Core/Exceptions/invalid_value_error.rb'
 
 module PlayerSymbolValidator
   def self.valid?(input)
-    raise NilReferenceError if input.nil?
-    raise InvalidValueError if input == ""
     pattern = /\A[^0-9\s]{1}\z/
-    match_data = pattern.match(input)
-    match_data != nil
+    validate_by.(input, pattern)
+  end
+
+  def self.validate_by
+    return lambda do |input, valid_choices_regex_pattern|
+      raise NilReferenceError if input.nil?
+      raise InvalidValueError if input == ""
+      match_data = valid_choices_regex_pattern.match(input)
+      match_data != nil
+    end
   end
 end
