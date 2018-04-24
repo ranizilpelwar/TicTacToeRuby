f875c9c3e0d16f6985d6092a5f19f60b5193f69b
Rani Zilpelwar <rani@8thlight.com>
2018-04-24 11:58:05 -0400

    remove deprecated method

--- TicTacToeRuby.Console/Validators/input_validator.rb
+++ TicTacToeRuby.Console/Validators/input_validator.rb
@@ -2,17 +2,6 @@ require_relative '../Output/console_writer.rb'
 require_relative '../Input/console_reader.rb'
 
 module InputValidator
-  def self.get_valid_selection(writer, reader, list_of_valid_choices)
-    input = ""
-    valid = false
-    while !valid
-      input = reader.read_input_ignore_empty
-      valid = valid?(input, list_of_valid_choices)
-      writer.display_message(MessageGenerator.invalid_selection_error) if !valid
-    end
-    result = input  
-  end
-
   def self.valid?
     return lambda { |input, valid_choices| valid = valid_choices.include?(input) }
   end
