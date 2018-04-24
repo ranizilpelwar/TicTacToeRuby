ea16a439900918d94ae23b041a104c5ff1677a5a
Rani Zilpelwar <rani@8thlight.com>
2018-04-24 12:08:06 -0400

    Remove unncessary file reference

--- TicTacToeRuby.Core/Validators/input_validator.rb
+++ TicTacToeRuby.Core/Validators/input_validator.rb
@@ -1,6 +1,3 @@
-require_relative '../Output/console_writer.rb'
-require_relative '../Input/console_reader.rb'
-
 module InputValidator
   def self.valid?
     return lambda { |input, valid_choices| valid = valid_choices.include?(input) }
