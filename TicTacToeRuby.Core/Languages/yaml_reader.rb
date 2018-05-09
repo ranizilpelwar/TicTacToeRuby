b13469db8bceb99494d3813a3543e1f3b11e5fd1
Rani Zilpelwar <rani@8thlight.com>
2018-05-09 11:47:37 -0400

    Reorder LOC to group similar properties together

--- TicTacToeRuby.Core/Languages/yaml_reader.rb
+++ TicTacToeRuby.Core/Languages/yaml_reader.rb
@@ -5,11 +5,11 @@ require_relative '../../TicTacToeRuby.Core/Exceptions/invalid_value_error.rb'
 module YAMLReader
   def self.read_data(file_path, property)
     raise NilReferenceError, "file_path" if file_path.nil?
-    file_path = File.dirname(__FILE__) + '/' + file_path
-    raise NilReferenceError, "property" if property.nil?
     raise InvalidValueError, "file_path" if file_path == ""
-    raise InvalidValueError, "property" if property == ""
+    file_path = File.dirname(__FILE__) + '/' + file_path
     raise InvalidValueError, "file_path = #{file_path}" if !(File.exist?(file_path))
+    raise NilReferenceError, "property" if property.nil?
+    raise InvalidValueError, "property" if property == ""
     yaml_file = YAML.load_file(file_path)
     data = yaml_file[property]
     raise InvalidValueError, property.to_s if data.nil?
