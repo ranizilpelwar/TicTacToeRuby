74e7fdf0aae6cd1fb83a98016d3cfd3acd791893
Rani Zilpelwar <rani@8thlight.com>
2018-05-09 11:36:37 -0400

    Update file path in tests

--- spec/Console/yaml_writer_spec.rb
+++ spec/Console/yaml_writer_spec.rb
@@ -5,10 +5,11 @@ require_relative '../../TicTacToeRuby.Core/Exceptions/invalid_value_error.rb'
 RSpec.describe "a yaml writer" do
   context "method called write_data" do
     it "updates the yaml content when a valid file path and property name are provided" do
-      file_path = "spec/TestFiles/testing.en.yaml"
+      file_path = "../../spec/TestFiles/testing.en.yaml"
       property = "test_property"
       value = DateTime.now.to_s
       YAMLWriter.write_data(file_path, property, value)
+      file_path = File.dirname(__FILE__) + '/' + file_path
       yaml_file = YAML.load_file(file_path)
       actual_value = yaml_file[property]
       expect(actual_value).to eq(value)
@@ -22,14 +23,14 @@ RSpec.describe "a yaml writer" do
     end
 
     it "raises a NilReferenceError when property is nil" do
-      file_path = "spec/TestFiles/testing.en.yaml"
+      file_path = "../../spec/TestFiles/testing.en.yaml"
       property = nil
       value = DateTime.now.to_s
       expect{ YAMLWriter.write_data(file_path, property, value) }.to raise_error(NilReferenceError)
     end
 
     it "raises a NilReferenceError when value is nil" do
-      file_path = "spec/TestFiles/testing.en.yaml"
+      file_path = "../../spec/TestFiles/testing.en.yaml"
       property = "test_property"
       value = nil
       expect{ YAMLWriter.write_data(file_path, property, value) }.to raise_error(NilReferenceError)
@@ -43,28 +44,28 @@ RSpec.describe "a yaml writer" do
     end
 
     it "raises an InvalidValueError when property is empty" do
-      file_path = "spec/TestFiles/testing.en.yaml"
+      file_path = "../../spec/TestFiles/testing.en.yaml"
       property = ""
       value = DateTime.now.to_s
       expect{ YAMLWriter.write_data(file_path, property, value) }.to raise_error(InvalidValueError)
     end
 
     it "raises an InvalidValueError when value is empty" do
-      file_path = "spec/TestFiles/testing.en.yaml"
+      file_path = "../../spec/TestFiles/testing.en.yaml"
       property = "test_property"
       value = ""
       expect{ YAMLWriter.write_data(file_path, property, value) }.to raise_error(InvalidValueError)
     end
 
     it "raises an InvalidValueError when file is non-existent" do
-      file_path = "spec/TestFiles/testing1.en.yaml"
+      file_path = "../../spec/TestFiles/testing1.en.yaml"
       property = "test_property"
       value = DateTime.now.to_s
       expect{ YAMLWriter.write_data(file_path, property, value) }.to raise_error(InvalidValueError)
     end
 
     it "raises an InvalidValueError when nothing is written to the file" do
-      file_path = "spec/TestFiles/testing.en.yaml"
+      file_path = "../../spec/TestFiles/testing.en.yaml"
       property = "test_property"
       value = ""
       expect{ YAMLWriter.write_data(file_path, property, value) }.to raise_error(InvalidValueError)
