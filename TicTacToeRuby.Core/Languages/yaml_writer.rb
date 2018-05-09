8feaf50e9d767ca7495f27368b9060a3c242c5f2
Rani Zilpelwar <rani@8thlight.com>
2018-05-09 11:36:26 -0400

    Reordered the exception handling to group properties together and to check for nil before constructing the full file path

--- TicTacToeRuby.Core/Languages/yaml_writer.rb
+++ TicTacToeRuby.Core/Languages/yaml_writer.rb
@@ -5,14 +5,14 @@ require_relative '../../TicTacToeRuby.Core/Exceptions/invalid_value_error.rb'
 
 module YAMLWriter
   def self.write_data(file_path, property, value)
-    file_path = File.dirname(__FILE__) + '/' + file_path
     raise NilReferenceError, "file_path" if file_path.nil?
-    raise NilReferenceError, "property" if property.nil?
-    raise NilReferenceError, "value" if value.nil?
     raise InvalidValueError, "file_path" if file_path == ""
-    raise InvalidValueError, "property" if property == ""
-    raise InvalidValueError, "value" if value == ""
+    file_path = File.dirname(__FILE__) + '/' + file_path
     raise InvalidValueError, "file" if !(File.exist?(file_path))
+    raise NilReferenceError, "property" if property.nil?
+    raise InvalidValueError, "property" if property == ""
+    raise NilReferenceError, "value" if value.nil?
+    raise InvalidValueError, "value" if value == ""
     yaml_file = YAML.load_file(file_path)
     yaml_file[property] = value
     length_written = File.write(file_path, yaml_file.to_yaml)
